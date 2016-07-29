3b8d86b18fde17182470ddc0b7a2b5bc44451be9
vishal rajyaguru <vishal.rajyaguru@teradata.com>
2016-07-28 18:19:41 -0700

    cleaning up proxy object after connection is closed

--- client.py
+++ client.py
@@ -14,16 +14,16 @@ def main():
 	ns = Pyro4.locateNS()
 	for rem_server, rem_server_uri in ns.list(prefix="server.").items():
 		print("client {0} joining {1}".format(client.name, rem_server))
-		server = Pyro4.Proxy(rem_server_uri)
-		server.join(client.name)
+		with Pyro4.Proxy(rem_server_uri) as server:
+			server.join(client.name)
 		
-		print("client {0} requested server supported methods: {1}".format(client.name, server.list_methods()))
-		print("client {0} requested server machinetype: {1}".format(client.name, server.get_machine_type()))
-		print("client {0} requested server fan speed: {1}".format(client.name, server.get_fan_speed()))
-		print("client {0} requested server temps: {1}".format(client.name, server.get_temps()))
+			print("client {0} requested server supported methods: {1}".format(client.name, server.list_methods()))
+			print("client {0} requested server machinetype: {1}".format(client.name, server.get_machine_type()))
+			print("client {0} requested server fan speed: {1}".format(client.name, server.get_fan_speed()))
+			print("client {0} requested server temps: {1}".format(client.name, server.get_temps()))
 		
-		print("client {0} leaving {1}".format(client.name, rem_server))
-		server.leave(client.name)
+			print("client {0} leaving {1}".format(client.name, rem_server))
+			server.leave(client.name)
 		
 		
 if __name__ == "__main__":
