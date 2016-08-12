2d8a4a48bd1c226845242db56b968dfce32ba634
vishal rajyaguru <vishal.rajyaguru@teradata.com>
2016-08-11 23:45:28 -0700

    implemented methods to get node sensor data using ipmitool and dmidecode

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import Pyro4
 import socket
 import ConfigParser
 import io
-
+import os
 
 class NodeServer(object):
 	def __init__(self):
@@ -17,17 +17,18 @@ class NodeServer(object):
 		self.hmac_key = ""
 		self.hmac_key_ns = ""
 		self.methods = ["get_fan_speed", "get_temps", "get_machine_type"]
-		self.fanspeed = ["8400RPM", "8400RPM", "8160RPM", "7680RPM","7440RPM","7560RPM"]
-		self.temps = ["24degrees C", "32degrees C"]
-		self.machinetype = "Dell PowerEdge R730"
+		self.fanspeed = []
+		self.temps = []
+		self.machinetype = ""
 		self.attached_clients = []
 
 	def list_clients(self):
-		print("PRIVATE-FUNC: server {0} client list: {1}".format(self.name, self.attached_clients))
+		print("server {0} client list: {1}".format(self.name, self.attached_clients))
 
 	@Pyro4.expose	
-	def list_methods(self):
-		print("server {0} received request to list supported methods.".format(self.name))
+	def list_methods(self, client):
+		print("server {0} received request to list supported methods from client {1}.".format(self.name, client))
+		print("server {0} responding with: {1}".format(self.name, self.methods))
 		return self.methods
 		
 	@Pyro4.expose
@@ -43,19 +44,37 @@ class NodeServer(object):
 		self.list_clients()	
 	
 	@Pyro4.expose
-	def get_fan_speed(self):
-		print("server {0} received request for fan speed.".format(self.name))
+	def get_fan_speed(self, client):
+		print("server {0} received request for fan speed from client {1}.".format(self.name, client))
+		self.fanspeed = []
+		fans = os.popen("ipmitool -c sdr type Fan | awk -F , /'RPM'/'{print $2}'")
+		for f in fans.readlines():
+        		self.fanspeed.append(int(f.strip()))
+		print("server {0} responding with: {1}".format(self.name, self.fanspeed))
 		return self.fanspeed
-	
+
 	@Pyro4.expose
-	def get_temps(self):
-		print("server {0} received request for temp.".format(self.name))
+	def get_temps(self, client):
+		print("server {0} received request for temp from client {1}.".format(self.name, client))
+		self.temps = []
+		inlet = os.popen("ipmitool -c sdr type Temperature | awk -F , /'Inlet'/'{print $2}'").read()
+		exhaust = os.popen("ipmitool -c sdr type Temperature | awk -F , /'Exhaust'/'{print $2}'").read()
+		self.temps.append(int(inlet.strip()))
+		self.temps.append(int(exhaust.strip()))
+		print("server {0} responding with: {1}".format(self.name, self.temps))
 		return self.temps
 	
 	@Pyro4.expose
-	def get_machine_type(self):
-		print("server {0} received request for machinetype.".format(self.name))
+	def get_machine_type(self, client):
+		print("server {0} received request for machinetype from client {1}.".format(self.name, client))
+		if self.machinetype == "":
+			print("server {0} querying machinetype information.".format(self.name))
+        		manufacturer = os.popen("dmidecode --type 1 | awk -F : /'Manufacturer'/'{print $2}'").read()
+        		product_name = os. popen("dmidecode --type 1 | awk -F : /'Product Name'/'{print $2}'").read()
+        		self.machinetype = manufacturer.strip() + " " + product_name.strip()
+		print("server {0} responding with: {1}".format(self.name, self.machinetype))
 		return self.machinetype
+
 	  
 def main():
 	server = NodeServer()
@@ -66,7 +85,7 @@ def main():
 			server_config = file.read()
 		config = ConfigParser.RawConfigParser(allow_no_value=True)
 		config.readfp(io.BytesIO(server_config))
-		print("Server loaded configuration file {0}".format(server.conf))
+		print("server loaded configuration file {0}".format(server.conf))
 
 
 		# Read options from configuration file
@@ -74,39 +93,39 @@ def main():
 		try:
 			if(config.get("application", "name") != ""):
 				server.name = config.get("application", "name")
-	                    	print("Server using name \"{0}\"".format(server.name))
+	                    	print("server using name \"{0}\"".format(server.name))
                 except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-                        print("Server config option name not found, using default name \"{0}\"".format(server.name))
+                        print("server config option name not found, using default name \"{0}\"".format(server.name))
 
 
 		# Read ip address
 		try:
 			if(config.get("connection", "ip_addr") != ""):
 				server.ip_addr = config.get("connection", "ip_addr")
-				print("Server {0} using ip_addr \"{1}\"".format(server.name, server.ip_addr))
+				print("server {0} using ip_addr \"{1}\"".format(server.name, server.ip_addr))
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("Server {0} config option ip_addr not found, using default value \"{1}\"".format(server.name, server.ip_addr))  
+			print("server {0} config option ip_addr not found, using default value \"{1}\"".format(server.name, server.ip_addr))  
 		
 		
 		# Read server hmac key
 		try:
 			if(config.get("connection", "hmac_key") != ""):
 				server.hmac_key = config.get("connection", "hmac_key")
-				print("Server {0} using hmac_key \"{1}\"".format(server.name, server.hmac_key))
+				print("server {0} using hmac_key \"{1}\"".format(server.name, server.hmac_key))
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("Server {0} config option hmac_key not found, using default value \"{1}\"".format(server.name, server.hmac_key))
+			print("server {0} config option hmac_key not found, using default value \"{1}\"".format(server.name, server.hmac_key))
 
 
 		# Read nameserver hmac key
 		try:
 			if(config.get("connection", "hmac_key_ns") != ""):
 				server.hmac_key_ns = config.get("connection", "hmac_key_ns")
-				print("Server {0} using hmac_key_ns \"{1}\"".format(server.name, server.hmac_key_ns))
+				print("server {0} using hmac_key_ns \"{1}\"".format(server.name, server.hmac_key_ns))
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("Server {0} config option hmac_key_ns not found, using default value \"{1}\"".format(server.name, server.hmac_key_ns))
+			print("server {0} config option hmac_key_ns not found, using default value \"{1}\"".format(server.name, server.hmac_key_ns))
 
 	except IOError:
-		print("Server {0} configuration file \"{1}\" not found, will use default options".format(server.name, server.conf))
+		print("server {0} configuration file \"{1}\" not found, will use default options".format(server.name, server.conf))
 
 
 	Pyro4.config.HOST = server.ip_addr
@@ -115,7 +134,7 @@ def main():
 		server_uri = daemon.register(server)
 		with Pyro4.locateNS(hmac_key=server.hmac_key_ns) as ns:
 			ns.register("server."+server.name, server_uri)
-		print("Server {0} is running.".format(server.name))
+		print("server {0} is running.".format(server.name))
 		daemon.requestLoop()
 
 if __name__ == "__main__":
