8a6cd954a8f4151b00d4ee6dc4151d7505c2aabe
vishal rajyaguru <vishal.rajyaguru@teradata.com>
2016-09-14 22:41:40 -0700

    added logging module to write messages to log file with ability to specify logging level

--- client.py
+++ client.py
@@ -7,76 +7,103 @@ import ConfigParser
 import io
 import socket
 import os
+import sys
+import logging
 
 
 class Client(object):
 	def __init__(self):
 		self.name = socket.gethostname()
 		self.conf = "client.conf"
+		self.logfile = "client.log"
+		self.loglevel = "INFO"
 		self.hmac_key = ""
 		self.hmac_key_ns = ""
 
 		
 def main():
 	client = Client()
+	self_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+	logging_levels = {"INFO":20, "WARNING":30, "DEBUG":10, "CRITICAL":50, "ERROR":40}
 
 	try:
 		# Load the configuration file
-		self_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 		conf_location = os.path.join(self_location, client.conf)
-
-		with open(conf_location, "r") as file:
+                with open(conf_location, "r") as file:
                         client_config = file.read()
                 config = ConfigParser.RawConfigParser(allow_no_value=True)
                 config.readfp(io.BytesIO(client_config))
-                print("client loaded configuration file {0}".format(client.conf))
-
 
                 # Read options from configuration file
+		# Read log filename
+		try:
+			if(config.get("logging", "logfile") != ""):
+				client.logfile = config.get("logging", "logfile")
+		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+			print("client config option logfile not found, using default logging file \"{0}\"".format(client.logfile), file=sys.stderr)
+		logf_location = os.path.join(self_location, client.logfile)
+
+		# Read log level and convert into numeric value
+		logstr_info = ""
+		try:
+                        if(config.get("logging", "loglevel") != ""):
+                                loglevel = config.get("logging", "loglevel").upper()
+				if(logging_levels.has_key(loglevel)):
+					logstr_info = "client using log level \"" + loglevel + "\""
+					client.loglevel = loglevel
+				else:
+					logstr_info = "client config option loglevel \"" + loglevel + "\" not valid, using default logging level \"" + client.loglevel + "\""
+		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+			logstr_info = "client config option loglevel not found, using default logging level \"" + client.loglevel + "\""
+		numeric_loglevel = logging_levels[client.loglevel]
+		logging.basicConfig(filename=logf_location, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=numeric_loglevel)
+		logging.info("client loaded configuration file %s", client.conf)
+		logging.info(logstr_info)
+
 		# Read application name
 		try:
 			if(config.get("application", "name") != ""):
 				client.name = config.get("application", "name")
-				print("client using name \"{0}\"".format(client.name))
+				logging.debug("client using name \"%s\"", client.name)
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("client config option name not found, using default name \"{0}\"".format(client.name))
+			logging.debug("client config option name not found, using default name \"%s\"", client.name)
 
                 # Read server hmac key
                 try:
                         if(config.get("connection", "hmac_key") != ""):
                                 client.hmac_key = config.get("connection", "hmac_key")
-                                print("client {0} using hmac_key \"{1}\"".format(client.name, client.hmac_key))
+				logging.debug("client %s using hmac_key \"%s\"", client.name, client.hmac_key)
                 except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-                        print("client {0} config option hmac_key not found, using default value \"{1}\"".format(client.name, client.hmac_key))
-
+			logging.debug("client %s config option hmac_key not found, using default value \"%s\"", client.name, client.hmac_key)
 
                 # Read nameserver hmac key
                 try:
                         if(config.get("connection", "hmac_key_ns") != ""):
                                 client.hmac_key_ns = config.get("connection", "hmac_key_ns")
-                                print("client {0} using hmac_key_ns \"{1}\"".format(client.name, client.hmac_key_ns))
-                except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-                        print("client {0} config option hmac_key_ns not found, using default value \"{1}\"".format(client.name, client.hmac_key_ns))
-
+                                logging.debug("client %s using hmac_key_ns \"%s\"", client.name, client.hmac_key_ns)
+		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+			logging.debug("client %s config option hmac_key_ns not found, using default value \"%s\"", client.name, client.hmac_key_ns)
 
 	except IOError:
-                print("client {0} configuration file \"{1}\" not found, will use default options".format(client.name, client.conf))
-
+		logf_location = os.path.join(self_location, client.logfile)
+		numeric_loglevel = logging_levels[client.loglevel]
+                logging.basicConfig(filename=logf_location, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=numeric_loglevel)
+		logging.warning("client %s configuration file \"%s\" not found, will use default options", client.name, client.conf)
 
 
 	ns = Pyro4.locateNS(hmac_key=client.hmac_key_ns)
 	for rem_server, rem_server_uri in ns.list(prefix="server.").items():
-		print("client {0} joining {1}".format(client.name, rem_server))
+		logging.info("client %s joining %s", client.name, rem_server)
 		with Pyro4.Proxy(rem_server_uri) as server:
 			server._pyroHmacKey = client.hmac_key
 			server.join(client.name)
 		
-			print("client {0} requested server supported methods: {1}".format(client.name, server.list_methods(client.name)))
-			print("client {0} requested server machinetype: {1}".format(client.name, server.get_machine_type(client.name)))
-			print("client {0} requested server fan speed: {1}".format(client.name, server.get_fan_speed(client.name)))
-			print("client {0} requested server temps: {1}".format(client.name, server.get_temps(client.name)))
-		
-			print("client {0} leaving {1}".format(client.name, rem_server))
+			logging.debug("client %s requested server supported methods: %s", client.name, server.list_methods(client.name))
+			logging.debug("client %s requested server machinetype: %s", client.name, server.get_machine_type(client.name))
+			logging.debug("client %s requested server fan speed: %s", client.name, server.get_fan_speed(client.name))
+			logging.debug("client %s requested server temps: %s", client.name, server.get_temps(client.name))
+
+			logging.info("client %s leaving %s", client.name, rem_server)
 			server.leave(client.name)
 		
 		
--- server.py
+++ server.py
@@ -8,8 +8,11 @@ import socket
 import ConfigParser
 import io
 import os
+import sys
+import logging
 
 
+#@Pyro4.behavior(instance_mode="percall")
 class NodeServer(object):
 	def __init__(self):
 		self.name = socket.gethostname()
@@ -17,59 +20,61 @@ class NodeServer(object):
 		self.ip_addr = "127.0.0.1"
 		self.hmac_key = ""
 		self.hmac_key_ns = ""
+		self.logfile = "server.log"
+		self.loglevel = "DEBUG"
 		self.methods = ["get_fan_speed", "get_temps", "get_machine_type"]
 		self.machinetype = ""
 		self.attached_clients = []
 
 	def list_clients(self):
-		print("server {0} client list: {1}".format(self.name, self.attached_clients))
+		logging.debug("server %s client list: %s", self.name, self.attached_clients)
 
 	@Pyro4.expose	
 	def list_methods(self, client):
-		print("server {0} received request to list supported methods from client {1}.".format(self.name, client))
-		print("server {0} responding to {1} with: {2}".format(self.name, client, self.methods))
+		logging.debug("server %s received request to list supported methods from client %s.", self.name, client)
+		logging.debug("server %s responding to %s with: %s", self.name, client, self.methods)
 		return self.methods
 		
 	@Pyro4.expose
 	def join(self, client):
-		print("server {0} adding new client {1}".format(self.name, client))
+		logging.info("server %s adding new client %s", self.name, client)
 		self.attached_clients.append(client)
 		self.list_clients()	
 
 	@Pyro4.expose
 	def leave(self, client):
-		print("server {0} removing client {1}".format(self.name, client))
+		logging.info("server %s removing client %s", self.name, client)
 		self.attached_clients.remove(client)
 		self.list_clients()	
 	
 	@Pyro4.expose
 	def get_fan_speed(self, client):
-		print("server {0} received request for fan speed from client {1}.".format(self.name, client))
+		logging.info("server %s received request for fan speed from client %s.", self.name, client)
 		fanspeed = []
 		fans = os.popen("ipmitool -c sdr type Fan | awk -F , /'RPM'/'{print $2}'")
 		for f in fans.readlines():
-			fanspeed.append(int(f.strip()))
-		print("server {0} responding to {1} with: {2}".format(self.name, client,  fanspeed))
+                        fanspeed.append(int(f.strip()))
+		logging.debug("server %s responding to %s with: %s", self.name, client,  fanspeed)
 		return fanspeed
 
 	@Pyro4.expose
 	def get_temps(self, client):
-		print("server {0} received request for temp from client {1}.".format(self.name, client))
+		logging.info("server %s received request for temp from client %s.", self.name, client)
 		temps = []
 		inlet = os.popen("ipmitool -c sdr type Temperature | awk -F , /'Inlet'/'{print $2}'").read()
 		exhaust = os.popen("ipmitool -c sdr type Temperature | awk -F , /'Exhaust'/'{print $2}'").read()
 		temps.append(int(inlet.strip()))
 		temps.append(int(exhaust.strip()))
-		print("server {0} responding to {1} with: {2}".format(self.name, client, temps))
+		logging.debug("server %s responding to %s with: %s", self.name, client, temps)
 		return temps
 	
 	@Pyro4.expose
 	def get_machine_type(self, client):
-		print("server {0} received request for machinetype from client {1}.".format(self.name, client))
+		logging.info("server %s received request for machinetype from client %s.", self.name, client)
 		if self.machinetype == "":
-			print("server {0} querying machinetype information.".format(self.name))
-			manufacturer = os.popen("dmidecode --type 1 | awk -F : /'Manufacturer'/'{print $2}'").read().strip()
-			product_name = os. popen("dmidecode --type 1 | awk -F : /'Product Name'/'{print $2}'").read().strip()
+			logging.debug("server %s querying machinetype information.", self.name)
+                        manufacturer = os.popen("dmidecode --type 1 | awk -F : /'Manufacturer'/'{print $2}'").read().strip()
+                        product_name = os.popen("dmidecode --type 1 | awk -F : /'Product Name'/'{print $2}'").read().strip()
 			bbpn = os.popen("dmidecode --type 2 | awk -F : /'Product Name'/'{print $2}'").read().strip()
 			if manufacturer != "" and product_name != "":
 				self.machinetype = manufacturer + " " + product_name
@@ -77,63 +82,92 @@ class NodeServer(object):
 				self.machinetype = bbpn
 			else:
 				self.machinetype = "notdefined"
-		print("server {0} responding with: {1}".format(self.name, self.machinetype))
+		logging.debug("server %s responding with: %s", self.name, self.machinetype)
 		return self.machinetype
 
 	  
 def main():
 	server = NodeServer()
+	self_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+	logging_levels = {"INFO":20, "WARNING":30, "DEBUG":10, "CRITICAL":50, "ERROR":40}
 
 	try:
 		# Load the configuration file
-		self_location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-		conf_location = os.path.join(self_location, server.conf)
-
+                conf_location = os.path.join(self_location, server.conf)
 		with open(conf_location, "r") as file:
 			server_config = file.read()
 		config = ConfigParser.RawConfigParser(allow_no_value=True)
 		config.readfp(io.BytesIO(server_config))
-		print("server loaded configuration file {0}".format(server.conf))
 
 
 		# Read options from configuration file
+		# Read log filename
+		try:
+                        if(config.get("logging", "logfile") != ""):
+                                server.logfile = config.get("logging", "logfile")
+                except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+                        print("server config option logfile not found, using default logging file \"{0}\"".format(server.logfile), file=sys.stderr)
+                logf_location = os.path.join(self_location, server.logfile)
+
+
+		# Read log level and convert into numeric value
+		logstr_info = ""
+                try:
+                        if(config.get("logging", "loglevel") != ""):
+                                loglevel = config.get("logging", "loglevel").upper()
+                                if(logging_levels.has_key(loglevel)):
+                                        logstr_info = "server using log level \"" + loglevel + "\""
+                                        server.loglevel = loglevel
+                                else:
+                                        logstr_info = "server config option loglevel \"" + loglevel + "\" not valid, using default logging level \"" + server.loglevel + "\""
+                except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+                        logstr_info = "server config option loglevel not found, using default logging level \"" + server.loglevel + "\""
+                numeric_loglevel = logging_levels[server.loglevel]
+                logging.basicConfig(filename=logf_location, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=numeric_loglevel)
+                logging.info("server loaded configuration file %s", server.conf)
+                logging.info(logstr_info)
+
+
 		# Read application name
 		try:
 			if(config.get("application", "name") != ""):
 				server.name = config.get("application", "name")
-	                    	print("server using name \"{0}\"".format(server.name))
+	                        logging.debug("server using name \"%s\"", server.name)
                 except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-                        print("server config option name not found, using default name \"{0}\"".format(server.name))
+                        logging.debug("server config option name not found, using default name \"%s\"", server.name)
 
 
 		# Read ip address
 		try:
 			if(config.get("connection", "ip_addr") != ""):
 				server.ip_addr = config.get("connection", "ip_addr")
-				print("server {0} using ip_addr \"{1}\"".format(server.name, server.ip_addr))
+				logging.debug("server %s using ip_addr \"%s\"", server.name, server.ip_addr)
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("server {0} config option ip_addr not found, using default value \"{1}\"".format(server.name, server.ip_addr))  
+			logging.warning("server %s config option ip_addr not found, using default value \"%s\"", server.name, server.ip_addr)
 		
 		
 		# Read server hmac key
 		try:
 			if(config.get("connection", "hmac_key") != ""):
 				server.hmac_key = config.get("connection", "hmac_key")
-				print("server {0} using hmac_key \"{1}\"".format(server.name, server.hmac_key))
+				logging.debug("server %s using hmac_key \"%s\"", server.name, server.hmac_key)
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("server {0} config option hmac_key not found, using default value \"{1}\"".format(server.name, server.hmac_key))
+			logging.debug("server %s config option hmac_key not found, using default value \"%s\"", server.name, server.hmac_key)
 
 
 		# Read nameserver hmac key
 		try:
 			if(config.get("connection", "hmac_key_ns") != ""):
 				server.hmac_key_ns = config.get("connection", "hmac_key_ns")
-				print("server {0} using hmac_key_ns \"{1}\"".format(server.name, server.hmac_key_ns))
+				logging.debug("server %s using hmac_key_ns \"%s\"", server.name, server.hmac_key_ns)
 		except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-			print("server {0} config option hmac_key_ns not found, using default value \"{1}\"".format(server.name, server.hmac_key_ns))
+			logging.debug("server %s config option hmac_key_ns not found, using default value \"%s\"", server.name, server.hmac_key_ns)
 
 	except IOError:
-		print("server {0} configuration file \"{1}\" not found, will use default options".format(server.name, server.conf))
+		logf_location = os.path.join(self_location, server.logfile)
+                numeric_loglevel = logging_levels[server.loglevel]
+                logging.basicConfig(filename=logf_location, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=numeric_loglevel)
+		logging.warning("server %s configuration file \"%s\" not found, will use default options", server.name, server.conf)
 
 
 	Pyro4.config.HOST = server.ip_addr
@@ -142,7 +176,8 @@ def main():
 		server_uri = daemon.register(server)
 		with Pyro4.locateNS(hmac_key=server.hmac_key_ns) as ns:
 			ns.register("server."+server.name, server_uri)
-		print("server {0} is running.".format(server.name))
+		print("server {0} is running.".format(server.name), file=sys.stdout)
+		logging.info("server %s is running.", server.name)
 		daemon.requestLoop()
 
 if __name__ == "__main__":
